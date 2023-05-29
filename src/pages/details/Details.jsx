195b15e3521f6ed646f0a23ce2b2c6652baba60c
Fatih <16fay61@gmail.com>
2023-05-29 20:52:47 +0200

    aa

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,22 +1,23 @@
 import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Container, Card } from 'react-bootstrap';
 
 const Details = () => {
+  //*useParams, router ile gonderilen veriyi yakalamak icin kullanilir.
   const { namee } = useParams();
-  const navigate = useNavigate()
   const [ülke, setUlke] = useState([]);
 
   useEffect(() => {
     fetch(`https://restcountries.com/v3.1/name/${namee}`).then((res) => res.json()).then((data) => setUlke(data))
   }, [namee]);
 
+  //* Bu fonk. API den gelen googlemaps url si ile ayri bir sayfada buna erisim sagliyor.
   const handleMapClick = (mapUrl) => {
     window.open(mapUrl, '_blank');
   };
 
   return (
-    <div className="container text-center d-flex justify-content-center" style={{marginTop:"4.5rem"}}>
+    <div className="container text-center d-flex justify-content-center" style={{ marginTop: "4.5rem" }}>
       {ülke.map(a => {
         const { name, capital, currencies, languages, region, population, maps, translations, flags } = a;
         if (name.common === namee) {
@@ -66,17 +67,14 @@ const Details = () => {
                     </Card.Text>
                     <button className='btn btn-warning mb-1' onClick={() => handleMapClick(maps.googleMaps)}>View on Google Maps</button><br />
                     <button
-                  className="btn btn-warning"
-                  onClick={() => navigate("/")}
-                >
-                  HOME
-                </button>
+                      className="btn btn-warning"
+                      onClick={() => window.history.back()} 
+                    >
+                      BACK
+                    </button>
                   </Card.Body>
                 </Card>
               </Container>
-              <div className='p-3 '>
-                
-              </div>
             </div>
           );
         }
